a0e375bffe80e5766bd9f8eb8a5c2d3a234f93f1
derekw023 <derek@witt.me>
2022-10-06 00:22:12 -0400

    blech

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 pub fn vendor_iperf() {
     // Compile iperf using autotools with configuration to output a static library
-    let dst = autotools::Config::new("iperf")
-        .enable_static()
-        .insource(false)
-        .fast_build(true)
-        .build();
+    let dst =
+        autotools::Config::new(std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("iperf"))
+            .enable_static()
+            .config_option("with-ssl", None)
+            // .fast_build(true)
+            .build();
 
     // Explicitly search library directory
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
     // Add static library+deps to link list
     println!("cargo:rustc-link-lib=static=iperf");
+    println!("cargo:rustc-link-lib=static=sctp");
 }
